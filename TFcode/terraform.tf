6c2d5984109336475aeb8284e4d3de8d69561932
Le Tran Nhat Dang <dang12394@gmail.com>
2025-08-04 12:13:18 +0700

    add docker build and Terraform deploy

--- TFcode/terraform.tf
+++ TFcode/terraform.tf
@@ -1,3 +1,9 @@
 terraform {
   required_version = "~> 1.12.2"
+  backend "azurerm" {
+    resource_group_name  = "DevOps_group"
+    storage_account_name = "tfstatestore12394"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
